263e3b8ba2b3bbae2223cf56a8ddb2c4de7d12d4
cuiwang <417787204@qq.com>
2018-05-30 21:17:46 +0800

    add

--- src/app/core/startup/startup.service.ts
+++ src/app/core/startup/startup.service.ts
@@ -40,7 +40,7 @@ export class StartupService {
     return new Promise((resolve, reject) => {
       zip(
         this.httpClient.get(`assets/tmp/i18n/${this.i18n.defaultLang}.json`),
-        this.httpClient.get('assets/tmp/app-data.json'),
+        // this.httpClient.get('assets/tmp/app-data.json'),
       )
         .pipe(
           // 接收其他拦截器后产生的异常消息
@@ -52,8 +52,8 @@ export class StartupService {
         .subscribe(
           ([langData, appData]) => {
             // setting language data
-            this.translate.setTranslation(this.i18n.defaultLang, langData);
-            this.translate.setDefaultLang(this.i18n.defaultLang);
+            // this.translate.setTranslation(this.i18n.defaultLang, langData);
+            // this.translate.setDefaultLang(this.i18n.defaultLang);
             // application data
             const res: any = appData;
             let menuList = this.localStorageService.getLocalstorage(USER_INFO) ? JSON.parse(this.localStorageService.getLocalstorage(USER_INFO)).menuList : [];
@@ -72,15 +72,15 @@ export class StartupService {
             // console.log(data);
             // console.log(res);
             // 应用信息：包括站点名、描述、年份
-            this.settingService.setApp(res.app);
+            // this.settingService.setApp(res.app);
             // 用户信息：包括姓名、头像、邮箱地址
-            this.settingService.setUser(res.user);
+            // this.settingService.setUser(res.user);
             // ACL：设置权限为全量
-            this.aclService.setFull(true);
+            // this.aclService.setFull(true);
             // 初始化菜单
             this.menuService.add(menu);
             // 设置页面标题的后缀
-            this.titleService.suffix = res.app.name;
+            // this.titleService.suffix = res.app.name;
           },
           () => { },
           () => {
