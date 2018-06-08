4a0cbddf712948ea2af79b7b52f0738d75f5f0dc
cuiwang <417787204@qq.com>
2018-06-08 20:02:03 +0800

    add

--- src/app/shared/config/env.config.ts
+++ src/app/shared/config/env.config.ts
@@ -5,14 +5,14 @@ let config = env === 'development' ? {
   ENV: 'DEV',
   //  API: 'https://biz.juniuo.com/merchant/',
   // API1: 'https://biz.juniuo.com/',
-    API: 'http://pai.juniuo.com/merchant/',
-    API1: 'http://pai.juniuo.com/',
+    API: 'http://pay.juniuo.com/merchant/',
+    API1: 'http://pay.juniuo.com/',
   IMGURL: 'http://ts.diankayi.net/',
   OSS_IMAGE_URL: 'https://oss.juniuo.com/juniuo-pic/picture/juniuo/'
 } : {
   ENV: 'PRODAPI',
-  API: 'http://pai.juniuo.com/merchant/',
-  API1: 'http://pai.juniuo.com/',
+  API: 'http://pay.juniuo.com/merchant/',
+  API1: 'http://pay.juniuo.com/',
   // API: 'https://biz.juniuo.com/merchant/',
   // API1: 'https://biz.juniuo.com/',
   IMGURL: 'http://ts.diankayi.net/',
