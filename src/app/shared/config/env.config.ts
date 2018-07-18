3a57bdc1bf0a1807db58078b91c16227d313103b
cuiwang <417787204@qq.com>
2018-07-18 15:07:43 +0800

    add

--- src/app/shared/config/env.config.ts
+++ src/app/shared/config/env.config.ts
@@ -22,18 +22,18 @@ let config = env === 'development' ? {
   ENV: 'DEV',
   //  API: '//biz.juniuo.com/merchant/',
   // API1: '//biz.juniuo.com/',
-    API: '//b-test.juniuo.com/merchant/',
-    API1: '//b-test.juniuo.com/',
-  IMGURL: '//ts.diankayi.net/',
-  OSS_IMAGE_URL: '//oss.juniuo.com/juniuo-pic/picture/juniuo/'
+    API: 'http://b-test.juniuo.com/merchant/',
+    API1: 'http://b-test.juniuo.com/',
+  IMGURL: 'http://ts.diankayi.net/',
+  OSS_IMAGE_URL: 'http://oss.juniuo.com/juniuo-pic/picture/juniuo/'
 } : {
   ENV: 'PRODAPI',
-  API: '//b-test.juniuo.com/merchant/',
-  API1: '//b-test.juniuo.com/',
+  API: 'http://b-test.juniuo.com/merchant/',
+  API1: 'http://b-test.juniuo.com/',
   // API: '//biz.juniuo.com/merchant/',
   // API1: '//biz.juniuo.com/',
-  IMGURL: '//ts.diankayi.net/',
-  OSS_IMAGE_URL: '//oss.juniuo.com/juniuo-pic/picture/juniuo/'
+  IMGURL: 'http://ts.diankayi.net/',
+  OSS_IMAGE_URL: 'http://oss.juniuo.com/juniuo-pic/picture/juniuo/'
 };
 export const Config: any = JSON.parse(JSON.stringify(config));
 
