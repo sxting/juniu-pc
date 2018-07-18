47325c110d48cbfba915b0f2f605f627a460b30b
cuiwang <417787204@qq.com>
2018-07-18 15:05:20 +0800

    add

--- src/app/shared/config/env.config.ts
+++ src/app/shared/config/env.config.ts
@@ -3,37 +3,37 @@
 let env = 'development';
 // let config = env === 'development' ? {
 //   ENV: 'DEV',
-//   //  API: 'https://biz.juniuo.com/merchant/',
-//   // API1: 'https://biz.juniuo.com/',
-//     API: 'https://biz.juniuo.com/merchant/',
-//     API1: 'https://biz.juniuo.com/',
+//   //  API: '//biz.juniuo.com/merchant/',
+//   // API1: '//biz.juniuo.com/',
+//     API: '//biz.juniuo.com/merchant/',
+//     API1: '//biz.juniuo.com/',
 //   IMGURL: 'http://ts.diankayi.net/',
-//   OSS_IMAGE_URL: 'https://oss.juniuo.com/juniuo-pic/picture/juniuo/'
+//   OSS_IMAGE_URL: '//oss.juniuo.com/juniuo-pic/picture/juniuo/'
 // } : {
 //   ENV: 'PRODAPI',
-//   API: 'https://biz.juniuo.com/merchant/',
-//   API1: 'https://biz.juniuo.com/',
-//   // API: 'https://biz.juniuo.com/merchant/',
-//   // API1: 'https://biz.juniuo.com/',
+//   API: '//biz.juniuo.com/merchant/',
+//   API1: '//biz.juniuo.com/',
+//   // API: '//biz.juniuo.com/merchant/',
+//   // API1: '//biz.juniuo.com/',
 //   IMGURL: 'http://ts.diankayi.net/',
-//   OSS_IMAGE_URL: 'https://oss.juniuo.com/juniuo-pic/picture/juniuo/'
+//   OSS_IMAGE_URL: '//oss.juniuo.com/juniuo-pic/picture/juniuo/'
 // };
 let config = env === 'development' ? {
   ENV: 'DEV',
-  //  API: 'https://biz.juniuo.com/merchant/',
-  // API1: 'https://biz.juniuo.com/',
-    API: 'http://b-test.juniuo.com/merchant/',
-    API1: 'http://b-test.juniuo.com/',
-  IMGURL: 'http://ts.diankayi.net/',
-  OSS_IMAGE_URL: 'https://oss.juniuo.com/juniuo-pic/picture/juniuo/'
+  //  API: '//biz.juniuo.com/merchant/',
+  // API1: '//biz.juniuo.com/',
+    API: '//b-test.juniuo.com/merchant/',
+    API1: '//b-test.juniuo.com/',
+  IMGURL: '//ts.diankayi.net/',
+  OSS_IMAGE_URL: '//oss.juniuo.com/juniuo-pic/picture/juniuo/'
 } : {
   ENV: 'PRODAPI',
-  API: 'http://b-test.juniuo.com/merchant/',
-  API1: 'http://b-test.juniuo.com/',
-  // API: 'https://biz.juniuo.com/merchant/',
-  // API1: 'https://biz.juniuo.com/',
-  IMGURL: 'http://ts.diankayi.net/',
-  OSS_IMAGE_URL: 'https://oss.juniuo.com/juniuo-pic/picture/juniuo/'
+  API: '//b-test.juniuo.com/merchant/',
+  API1: '//b-test.juniuo.com/',
+  // API: '//biz.juniuo.com/merchant/',
+  // API1: '//biz.juniuo.com/',
+  IMGURL: '//ts.diankayi.net/',
+  OSS_IMAGE_URL: '//oss.juniuo.com/juniuo-pic/picture/juniuo/'
 };
 export const Config: any = JSON.parse(JSON.stringify(config));
 
