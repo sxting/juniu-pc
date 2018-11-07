e54f0168a1777dbfc6e4e0bd5d283553e6f03a7d
cuiwang <417787204@qq.com>
2018-11-07 10:20:47 +0800

    add

--- src/app/routes/cashFlow/shared/cashFlow.service.ts
+++ src/app/routes/cashFlow/shared/cashFlow.service.ts
@@ -3,27 +3,57 @@ import { Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
-import { _HttpClient } from "@delon/theme";
-import { FunctionUtil } from "@shared/funtion/funtion-util";
-import { Config } from "@shared/config/env.config";
+import { _HttpClient } from '@delon/theme';
+import { FunctionUtil } from '@shared/funtion/funtion-util';
+import { Config } from '@shared/config/env.config';
 
 @Injectable()
 export class CashFlowService {
-    constructor(private http: _HttpClient) { }
+  constructor(private http: _HttpClient) {}
 
-    api = Config.API;
-    api1 = Config.API + 'finance'; //银行
-    api2 = Config.API + 'member'; //会员
-   //美大流水  
-   orderStreamBatchQuery(data: any) {
+  api = Config.API;
+  api1 = Config.API + 'finance'; //银行
+  api2 = Config.API + 'order'; //会员
+  //美大流水
+  orderStreamBatchQuery(data: any) {
     let apiUrl = Config.API1 + 'xmd-service/tuangou/orderStreamBatchQuery.json';
-    return this.http.get(apiUrl, data)
-        .map((response: Response) => response)
-        .catch(error => {
-            return Observable.throw(error);
-        });
+    return this.http
+      .get(apiUrl, data)
+      .map((response: Response) => response)
+      .catch(error => {
+        return Observable.throw(error);
+      });
+  }
+
+  //消费流水
+  consumeRecords(data: any) {
+    let apiUrl = Config.API + 'order/consume/records.json';
+    return this.http
+      .get(apiUrl, data)
+      .map((response: Response) => response)
+      .catch(error => {
+        return Observable.throw(error);
+      });
+  }
+
+  recordStatistics(data: any) {
+    let apiUrl = Config.API + 'order/consume/records/statistics.json';
+    return this.http
+      .get(apiUrl, data)
+      .map((response: Response) => response)
+      .catch(error => {
+        return Observable.throw(error);
+      });
+  }
+
+  // 营收报表订单详情
+  revenuetOrderDetail(data: any) {
+    let apiUrl = this.api2 + '/order/' + data + '/detail.json';
+    return this.http
+      .get(apiUrl)
+      .map((response: Response) => response)
+      .catch(error => {
+        return Observable.throw(error);
+      });
+  }
 }
-
-}
-
-
