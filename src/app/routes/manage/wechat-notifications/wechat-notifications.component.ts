fb0234cdb6704696d542a373fbe1aeeb6f4dddb3
cuiwang <417787204@qq.com>
2018-11-16 10:48:40 +0800

    add

--- src/app/routes/manage/wechat-notifications/wechat-notifications.component.ts
+++ src/app/routes/manage/wechat-notifications/wechat-notifications.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { _HttpClient, TitleService } from '@delon/theme';
-import { ManageService} from "../shared/manage.service";
+import { ManageService } from '../shared/manage.service';
 import { NzMessageService, NzModalService } from 'ng-zorro-antd';
 import { Router, ActivatedRoute } from '@angular/router';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
@@ -13,149 +13,171 @@ import { FunctionUtil } from '@shared/funtion/funtion-util';
   templateUrl: './wechat-notifications.component.html',
 })
 export class WechatNotificationsComponent implements OnInit {
+  form: any;
+  formData: any;
+  reportTypes: any[] = [
+    { name: '只接收自己的工作报表', value: 'WORK_REPORT_ONESELF' },
+    { name: '接收门店工作报表', value: 'WORK_REPORT_STORE' },
+  ]; //到店通知
+  staffId: string = '';
+  loading: boolean = false;
+  submitting: boolean = false;
+  qrcodeImages: string = ''; //二维码地址
+  WXImages: string = ''; //微信头像
+  WXname: string = ''; //微信名字
+  openid: any;
+  radioValue = 'WORK_REPORT_STORE';
+  timer: any;
+  constructor(
+    private http: _HttpClient,
+    private fb: FormBuilder,
+    private manageService: ManageService,
+    private modalSrv: NzModalService,
+    private titleSrv: TitleService,
+    private localStorageService: LocalStorageService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private msg: NzMessageService,
+  ) {}
+  ngOnDestroy() {
+    let self = this;
+    clearInterval(self.timer);
+  }
+  ngOnInit() {
+    let self = this;
+    this.staffId = this.route.snapshot.params['staffId']; //员工ID
+    this.faceQRcode(this.staffId); //微信二维码
+  }
 
-    form: any;
-    formData: any;
-    reportTypes: any[] = [{name: '只接收自己的工作报表',value:'WORK_REPORT_ONESELF'},{name: '接收门店工作报表',value:'WORK_REPORT_STORE'}];//到店通知
-    storeNotice: any[] = [{name: '只接收自己顾客的到店通知',value:'CUSTOMER_VISIT_ONESELF'},{name: '接收门店所有顾客到店通知',value:'CUSTOMER_VISIT_ALL_STORE'}];//报表类型
-    staffId: string = '';
-    loading: boolean = false;
-    submitting: boolean = false;
-    qrcodeImages: string = '';//二维码地址
-    openid:any;
-    radioValue:any;
-    constructor(
-        private http: _HttpClient,
-        private fb: FormBuilder,
-        private manageService: ManageService,
-        private modalSrv: NzModalService,
-        private titleSrv: TitleService,
-        private localStorageService: LocalStorageService,
-        private router: Router,
-        private route: ActivatedRoute,
-        private msg: NzMessageService
-    ) { }
-
-    ngOnInit() {
-        let self = this;
-        this.staffId = this.route.snapshot.params['staffId'];//员工ID
-        this.faceQRcode(this.staffId);//微信二维码
-        this.titleSrv.setTitle('微信推送');
-        this.formData = {
-            reportType: [self.reportTypes[0].value, [Validators.required]],
-            receiveType: [self.storeNotice[0].value, [Validators.required]]
-        };
-        this.form = self.fb.group(self.formData);
-    }
-
-    //二维码
-    faceQRcode(staffId: string){
-        let self = this;
-        this.loading = true;
-        let batchQuery =  {
-            staffId: staffId
-        };
-        this.manageService.faceQRcode(batchQuery).subscribe(
-            (res: any) => {
-                if (res.success) {
-                    this.loading = false;
-                    self.qrcodeImages = res.data;
-                    self.wechatPushConfigCheck(self.staffId);//查询微信推送设置
-                } else {
-                    this.modalSrv.error({
-                        nzTitle: '温馨提示',
-                        nzContent: res.errorInfo
-                    });
-                }
-            },
-            error => {
-                FunctionUtil.errorAlter(error);
-            }
-        );
-    }
-
-    //查询微信公众号推送配置
-    wechatPushConfigCheck(staffId: string){
-        let self = this;
-        this.loading = true;
-        let batchQuery =  {
-            timestamp: new Date().getTime(),
-            staffId: staffId
-        };
-        this.manageService.wechatPushConfig(batchQuery).subscribe(
-            (res: any) => {
-                if (res.success) {
-                    this.loading = false;
-                    self.openid  = res.data.openid;
-                    // reportTypes: any[] = [{name: '只接收自己的工作报表',value:'WORK_REPORT_ONESELF'},{name: '接收门店工作报表',value:'WORK_REPORT_STORE'}];//到店通知
-                    // storeNotice: any[] = [{name: '只接收自己顾客的到店通知',value:'CUSTOMER_VISIT_ONESELF'},{name: '接收门店所有顾客到店通知',value:'CUSTOMER_VISIT_ALL_STORE'}];//报表类型
-                    let reportTypes;
-                    let storeNotice;
-                    let pushTypeList = res.data.pushTypeList;
-                    if(pushTypeList.length != 0){
-                        pushTypeList.forEach(function (item: any) {
-                            reportTypes = item === 'WORK_REPORT_ONESELF'? 'WORK_REPORT_ONESELF' : 'WORK_REPORT_STORE';
-                            storeNotice = item === 'CUSTOMER_VISIT_ONESELF'? 'CUSTOMER_VISIT_ONESELF' : 'CUSTOMER_VISIT_ALL_STORE';
-                        });
-                    }else {
-                        reportTypes = self.reportTypes[0].value;
-                        storeNotice = self.storeNotice[0].value;
-                    }
-                    self.formData = {
-                        reportType: [ reportTypes, [Validators.required]],
-                        receiveType: [ storeNotice, [Validators.required]]
-                    };
-                    this.form = self.fb.group(self.formData);
-
-                } else {
-                    this.modalSrv.error({
-                        nzTitle: '温馨提示',
-                        nzContent: res.errorInfo
-                    });
-                }
-            },
-            error => {
-                FunctionUtil.errorAlter(error);
-            }
-        );
-    }
-
-    // 提交
-    submit(){
-        let self = this;
-        for (const i in this.form.controls) {
-            this.form.controls[ i ].markAsDirty();
-            this.form.controls[ i ].updateValueAndValidity();
+  //二维码
+  faceQRcode(staffId: string) {
+    let self = this;
+    this.loading = true;
+    let batchQuery = {
+      staffId: staffId,
+    };
+    this.manageService.faceQRcode(batchQuery).subscribe(
+      (res: any) => {
+        if (res.success) {
+          this.loading = false;
+          self.qrcodeImages = res.data;
+          self.wechatPushConfigCheck(self.staffId); //查询微信推送设置
+        } else {
+          this.modalSrv.error({
+            nzTitle: '温馨提示',
+            nzContent: res.errorInfo,
+          });
         }
-        if (this.form.invalid) return;
-        this.submitting = true;
+      },
+      error => {
+        FunctionUtil.errorAlter(error);
+      },
+    );
+  }
 
-        let reportTypes = this.form.controls.reportType.value;
-        let receiveType = this.form.controls.receiveType.value;
-        let params = {
-            pushTypeList: [ reportTypes, receiveType ],
-            staffId: this.staffId,
-            timestamp: new Date().getTime()
-        };
-        this.manageService.setPushWechat(params).subscribe(
-            (res: any) => {
-                if (res.success) {
-                    setTimeout(() => {
-                        self.submitting = false;
-                        self.msg.success(`微信推送配置成功`);
-                        self.router.navigate(['/manage/staff/list']);
-                    }, 1000);
-                } else {
-                    this.modalSrv.error({
-                        nzTitle: '温馨提示',
-                        nzContent: res.errorInfo
-                    });
-                }
-            },
-            (error) => {
-                this.msg.warning(error)
-            }
-        );
-    }
-
+  //查询微信公众号推送配置
+  wechatPushConfigCheck(staffId: string) {
+    let self = this;
+    this.loading = true;
+    let batchQuery = {
+      timestamp: new Date().getTime(),
+      staffId: staffId,
+    };
+    this.manageService.wechatPushConfig(batchQuery).subscribe(
+      (res: any) => {
+        if (res.success) {
+          this.loading = false;
+          // reportTypes: any[] = [{name: '只接收自己的工作报表',value:'WORK_REPORT_ONESELF'},{name: '接收门店工作报表',value:'WORK_REPORT_STORE'}];//到店通知
+          // storeNotice: any[] = [{name: '只接收自己顾客的到店通知',value:'CUSTOMER_VISIT_ONESELF'},{name: '接收门店所有顾客到店通知',value:'CUSTOMER_VISIT_ALL_STORE'}];//报表类型
+          let reportTypes;
+          let storeNotice;
+          let pushTypeList = res.data.pushTypeList;
+          if (pushTypeList.length === 0) {
+            reportTypes = 'C';
+          } else {
+            reportTypes = pushTypeList[0];
+          }
+          this.radioValue = reportTypes;
+          this.openid = res.data.openid;
+          if (!this.openid) {
+            this.timer = setTimeout(() => {
+                self.wechatPushConfigCheck(self.staffId);//查询微信推送设置
+            }, 2000);
+          } else {
+            this.WXImages = res.data.image;
+            this.WXname = res.data.nickName;
+          }
+          if (this.openid && this.timer) {
+            clearInterval(self.timer);
+          }
+        } else {
+          this.modalSrv.error({
+            nzTitle: '温馨提示',
+            nzContent: res.errorInfo,
+          });
+        }
+      },
+      error => {
+        FunctionUtil.errorAlter(error);
+      },
+    );
+  }
+  //解绑
+  jiebang() {
+    let self = this;
+    let params = {
+      staffId: this.staffId,
+      timestamp: new Date().getTime(),
+      platform: 'WECHAT_PUB',
+    };
+    self.submitting = true;
+    this.manageService.unbindingPlatform(params).subscribe(
+      (res: any) => {
+        self.submitting = false;
+        if (res.success) {
+          self.msg.success(`微信解绑成功`);
+          self.router.navigate(['/manage/staff/list']);
+        } else {
+          this.modalSrv.error({
+            nzTitle: '温馨提示',
+            nzContent: res.errorInfo,
+          });
+        }
+      },
+      error => {
+        this.msg.warning(error);
+      },
+    );
+  }
+  //切换接收类型
+  radioChange(event) {
+    let self = this;
+    this.submitting = true;
+    let reportTypes = event === 'C'?[]:[event];
+    let params = {
+      pushTypeList: reportTypes,
+      staffId: this.staffId,
+      timestamp: new Date().getTime(),
+    };
+    this.manageService.setPushWechat(params).subscribe(
+      (res: any) => {
+        self.submitting = false;
+        if (res.success) {
+          self.msg.success(`微信推送配置成功`);
+        } else {
+          this.modalSrv.error({
+            nzTitle: '温馨提示',
+            nzContent: res.errorInfo,
+          });
+        }
+      },
+      error => {
+        this.msg.warning(error);
+      },
+    );
+  }
+  // 提交
+  submit() {
+    
+  }
 }
