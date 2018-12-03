ee085d6f5ec92b635751bd5b5e3c25138eaf44cc
cuiwang <417787204@qq.com>
2018-12-03 17:54:48 +0800

    add

--- src/app/shared/config/env.config.ts
+++ src/app/shared/config/env.config.ts
@@ -1,24 +1,24 @@
 
 // import { EnvConfig } from 'tools/env/env-config.interface';
 let env = 'development';
-// let config = env === 'development' ? {
-//   ENV: 'DEV',
-//   //  API: '//biz.juniuo.com/merchant/',
-//   // API1: '//biz.juniuo.com/',
-//     API: 'https://biz.juniuo.com/merchant/',
-//     API1: 'https://biz.juniuo.com/',
-//   IMGURL: 'http://ts.diankayi.net/',
-//   OSS_IMAGE_URL: '//oss.juniuo.com/juniuo-pic/picture/juniuo/'
-// } : {
-//   ENV: 'PRODAPI',
-//   API: 'https://biz.juniuo.com/merchant/',
-//   API1: 'https://biz.juniuo.com/',
-//   // API: '//biz.juniuo.com/merchant/',
-//   // API1: '//biz.juniuo.com/',
-//   IMGURL: 'http://ts.diankayi.net/',
-//   OSS_IMAGE_URL: '//oss.juniuo.com/juniuo-pic/picture/juniuo/'
-// };
 let config = env === 'development' ? {
+  ENV: 'DEV',
+  //  API: '//biz.juniuo.com/merchant/',
+  // API1: '//biz.juniuo.com/',
+    API: 'https://biz.juniuo.com/merchant/',
+    API1: 'https://biz.juniuo.com/',
+  IMGURL: 'http://ts.diankayi.net/',
+  OSS_IMAGE_URL: '//oss.juniuo.com/juniuo-pic/picture/juniuo/'
+} : {
+  ENV: 'PRODAPI',
+  API: 'https://biz.juniuo.com/merchant/',
+  API1: 'https://biz.juniuo.com/',
+  // API: '//biz.juniuo.com/merchant/',
+  // API1: '//biz.juniuo.com/',
+  IMGURL: 'http://ts.diankayi.net/',
+  OSS_IMAGE_URL: '//oss.juniuo.com/juniuo-pic/picture/juniuo/'
+};
+let config2 = env === 'development' ? {
   ENV: 'DEV',
   //  API: '//biz.juniuo.com/merchant/',
   // API1: '//biz.juniuo.com/',
@@ -36,4 +36,5 @@ let config = env === 'development' ? {
   OSS_IMAGE_URL: 'http://fdfs.juniuo.com/juniuo-pic/picture/juniuo/',
   OSS_VIDEO_URL: 'https://juniuo-pic.oss-cn-beijing.aliyuncs.com/picture/201811/28/'  
 };
-export const Config: any = JSON.parse(JSON.stringify(config));
+console.log(window.location.host.indexOf('biz.juniuo'))
+export const Config: any = JSON.parse(JSON.stringify(window.location.host.indexOf('biz.juniuo')>-1?config:config2));
