a7395b96b7ceca037824efb6519184e94d8e4ce5
cuiwang <417787204@qq.com>
2018-12-06 15:28:23 +0800

    add

--- src/app/routes/product/add-new-items/add-new-items.component.ts
+++ src/app/routes/product/add-new-items/add-new-items.component.ts
@@ -536,7 +536,7 @@ export class AddNewItemsComponent implements OnInit {
         let data = {
             menuId: '900502B1',
             timestamp: new Date().getTime(),
-            storeId:''
+            storeId:this.storeId || ''
           };
           this.uploadService.menuRoute(data).then((result: any) => {
             if(result){
