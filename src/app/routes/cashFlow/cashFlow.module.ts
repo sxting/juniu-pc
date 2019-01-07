fbb53154eac783b6874bcb2c933b8eb5e63c5149
liuzuoli <liuzuoliabc@juniuo.com>
2019-01-07 19:54:06 +0800

    收单流水

--- src/app/routes/cashFlow/cashFlow.module.ts
+++ src/app/routes/cashFlow/cashFlow.module.ts
@@ -8,6 +8,7 @@ import { MeidaFlowComponent } from './meidaFlow/meidaFlow.component';
 import { KoubeiFlowComponent } from './koubeiFlow/koubeiFlow.component';
 import { ProgramFlowComponent } from './program-flow/program-flow.component';
 import { PaycodeFlowComponent } from './paycode-flow/paycode-flow.component';
+import { CheckoutService } from '../checkout/shared/checkout.service';
 
 
 @NgModule({
@@ -23,6 +24,6 @@ import { PaycodeFlowComponent } from './paycode-flow/paycode-flow.component';
     ProgramFlowComponent,
     PaycodeFlowComponent
   ],
-  providers: [CashFlowService]
+  providers: [CashFlowService, CheckoutService]
 })
 export class CashFlowModule { }
