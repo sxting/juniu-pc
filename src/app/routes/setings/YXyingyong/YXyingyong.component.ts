ffc1434eca343894db15c41b8803aac6670963b1
cuiwang <417787204@qq.com>
2019-02-20 18:02:42 +0800

    add

--- src/app/routes/setings/YXyingyong/YXyingyong.component.ts
+++ src/app/routes/setings/YXyingyong/YXyingyong.component.ts
@@ -9,7 +9,11 @@ import {NzModalService} from "ng-zorro-antd";
   styleUrls: ['./YXyingyong.component.less']
 })
 export class YXyingyongComponent implements OnInit {
-  tplArr=[1]
+  tplArr=[1];
+  dataList = [];
+  storeId = '';
+  goumaiList= [];
+  goumaiArr = [];
   constructor(
     private setingsService: SetingsService,
     private modalSrv: NzModalService,
@@ -17,8 +21,63 @@ export class YXyingyongComponent implements OnInit {
 
 
   ngOnInit() {
+    this.getPackageBatchList();
+  }
+  /*++++++++++分界线=========*/
+  getPackageBatchList() {
+    let data = {
+      storeId: this.storeId,
+      packageType:'PLUGIN'
+    };
+    this.setingsService.getPackageBatchList(data).subscribe(
+      (res: any) => {
+        if(res.success) {
+          this.dataList = res.data.items;
+          /*
+          * selectable 控制可不可点
+          * useState true 续费 false 升级
+          * */
+        } else {
+          this.modalSrv.error({
+            nzTitle: '温馨提示',
+            nzContent: res.errorInfo
+          });
+        }
+      }
+    )
   }
 
- 
+  getPurchaseRecordFun(packageId,tpl){
+    let data = {
+      packageId: packageId,
+      pageNo: 1 ,
+      pageSize: 100
+    };
+    this.setingsService.getPurchaseRecord(data).subscribe(
+      (res: any) => {
+        if(res.success) {
+          this.goumaiArr = res.data.items;
+          this.modalSrv.create({
+            nzTitle: '购买记录',
+            nzContent: tpl,
+            nzWidth: '500px',
+            nzOkText: null,
+            nzOnCancel: () => {
+            },
+          });
+        } else {
+          this.modalSrv.error({
+            nzTitle: '温馨提示',
+            nzContent: res.errorInfo
+          });
+        }
+      }
+    )
+  }
+  goumaiFun(tpl,packageId){
+    this.getPurchaseRecordFun(packageId,tpl)
+    let self = this;
+    
+  }
 
 }
